b6e5b5dd25ef0149cf355c107ee50d02fb7473cd
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-13 16:03:05 +0300

    Create config

--- src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
+++ src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
@@ -1,18 +1,43 @@
 package top.offsetmonkey538.githubresourcepackmanager;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.offsetmonkey538.githubresourcepackmanager.config.ModConfig;
+import top.offsetmonkey538.monkeylib538.config.ConfigManager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class GithubResourcepackManager implements DedicatedServerModInitializer {
 	public static final String MOD_ID = "github-resourcepack-manager";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final Path RESOURCEPACK_FOLDER = FabricLoader.getInstance().getGameDir().resolve("resourcepack");
+	public static final Path GIT_FOLDER = RESOURCEPACK_FOLDER.resolve("git");
+	public static final Path OUTPUT_FOLDER = RESOURCEPACK_FOLDER.resolve("output");
+
+
+	public static ModConfig config;
+
 	@Override
 	public void onInitializeServer() {
+		config = ConfigManager.init(new ModConfig(), LOGGER::error);
 
+		if (config.githubUrl == null || config.githubUsername == null || config.githubToken == null) {
+			LOGGER.error("Please fill in the config file!");
+			throw new RuntimeException("Please fill in the config file!");
+		}
+
+		try {
+			if (!OUTPUT_FOLDER.toFile().exists()) Files.createDirectories(OUTPUT_FOLDER);
+		} catch (IOException e) {
+			LOGGER.error("Couldn't create output directory!");
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static Identifier id(String path) {
---
+++ src/main/java/top/offsetmonkey538/githubresourcepackmanager/config/ModConfig.java
@@ -0,0 +1,22 @@
+package top.offsetmonkey538.githubresourcepackmanager.config;
+
+import blue.endless.jankson.Comment;
+import top.offsetmonkey538.monkeylib538.config.Config;
+
+import static top.offsetmonkey538.githubresourcepackmanager.GithubResourcepackManager.MOD_ID;
+
+public class ModConfig extends Config {
+    public int serverPort = 8080;
+    public String webhookPath = "/webhook";
+    public String githubRef = "refs/heads/master";
+    public String githubUrl = null;
+    public String githubUsername = null;
+
+    @Comment("PLEASE DON'T SHARE THIS WITH ANYONE EVER")
+    public String githubToken = null;
+
+    @Override
+    protected String getName() {
+        return MOD_ID;
+    }
+}
