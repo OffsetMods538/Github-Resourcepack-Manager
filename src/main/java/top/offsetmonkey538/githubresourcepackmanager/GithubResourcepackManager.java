caa1c3b6b004eedc5123378e586f6ac80d85a51f
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-13 16:03:50 +0300

    Implement method for cloning and updating the git repository

--- src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
+++ src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
@@ -3,6 +3,11 @@ package top.offsetmonkey538.githubresourcepackmanager;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.offsetmonkey538.githubresourcepackmanager.config.ModConfig;
@@ -37,6 +42,46 @@ public class GithubResourcepackManager implements DedicatedServerModInitializer 
 		} catch (IOException e) {
 			LOGGER.error("Couldn't create output directory!");
 			throw new RuntimeException(e);
+		}
+
+		updatePack();
+	}
+
+	public static void updatePack() {
+		updateRepository();
+	}
+
+	public static void updateRepository() {
+		final CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(config.githubUsername, config.githubToken);
+
+		if (GIT_FOLDER.toFile().exists()) {
+			try	(Git git = Git.open(GIT_FOLDER.toFile())) {
+				final PullResult result = git.pull()
+						.setCredentialsProvider(credentialsProvider)
+						.call();
+				if (result.isSuccessful()) {
+					LOGGER.info("Successfully updated repository!");
+					return;
+				}
+				LOGGER.info("Failed to update repository!");
+			} catch (GitAPIException e) {
+				LOGGER.error("Failed to update repository!", e);
+			} catch (IOException e) {
+				LOGGER.error("Failed to open repository!", e);
+			}
+
+			return;
+		}
+
+		try {
+			Git git = Git.cloneRepository()
+					.setURI(config.githubUrl.endsWith(".git") ? config.githubUrl : config.githubUrl + ".git")
+					.setDirectory(GIT_FOLDER.toFile())
+					.setCredentialsProvider(credentialsProvider)
+					.call();
+			git.close();
+		} catch (GitAPIException e) {
+			LOGGER.error("Failed to clone repository!", e);
 		}
 	}
 
