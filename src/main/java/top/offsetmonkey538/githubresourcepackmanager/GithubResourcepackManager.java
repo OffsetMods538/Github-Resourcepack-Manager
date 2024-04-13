e0e3abff4b93dbc0ae59dc969130001d3c37a1f6
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-04-13 15:30:27 +0300

    Dedicated server only

--- src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
+++ src/main/java/top/offsetmonkey538/githubresourcepackmanager/GithubResourcepackManager.java
@@ -1,17 +1,18 @@
 package top.offsetmonkey538.githubresourcepackmanager;
 
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GithubResourcepackManager implements ModInitializer {
+public class GithubResourcepackManager implements DedicatedServerModInitializer {
 	public static final String MOD_ID = "github-resourcepack-manager";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		// Do stuff
+	public void onInitializeServer() {
+
 	}
 
 	public static Identifier id(String path) {
