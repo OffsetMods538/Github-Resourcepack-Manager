f87715287ffb7d4cc25c0f3ef3c661294c6ff64c
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-06-02 16:42:27 +0300

    Write custom resource manager for handling http requests
    The custom resource manager always returns an instance of the custom resource class pointing to the current output file.
    The custom resource overrides the 'getName' function to always return the path that was requested.
    
    For example if you go to '123.45.67.89:8080/cool/stuff/myServerPack.zip', it will download the latest pack and name it 'myServerPack.zip'

--- src/main/java/top/offsetmonkey538/githubresourcepackmanager/networking/MainHttpHandler.java
+++ src/main/java/top/offsetmonkey538/githubresourcepackmanager/networking/MainHttpHandler.java
@@ -3,13 +3,12 @@ package top.offsetmonkey538.githubresourcepackmanager.networking;
 import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.handlers.resource.PathResourceManager;
 
 import static top.offsetmonkey538.githubresourcepackmanager.GithubResourcepackManager.*;
 
 public class MainHttpHandler implements HttpHandler {
     private final HttpHandler webhookHandler = new WebhookHttpHandler();
-    private final HttpHandler fileHandler = Handlers.resource(new PathResourceManager(OUTPUT_FOLDER, 100));
+    private final HttpHandler fileHandler = Handlers.resource(new PackResourceManager(OUTPUT_FOLDER));
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
