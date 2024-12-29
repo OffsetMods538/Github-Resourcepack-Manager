8d065cdf2ac9aa90eb99e73734368911162355c9
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-12-29 23:30:32 +0100

    Fix initializer

--- fabric/src/main/java/top/offsetmonkey538/githubresourcepackmanager/platform/fabric/FabricPlatformMain.java
+++ fabric/src/main/java/top/offsetmonkey538/githubresourcepackmanager/platform/fabric/FabricPlatformMain.java
@@ -1,11 +1,18 @@
 package top.offsetmonkey538.githubresourcepackmanager.platform.fabric;
 
+import net.fabricmc.api.DedicatedServerModInitializer;
 import org.slf4j.Logger;
 import top.offsetmonkey538.githubresourcepackmanager.platform.PlatformMain;
 
 import java.nio.file.Path;
 
-public class FabricPlatformMain implements PlatformMain {
+public class FabricPlatformMain implements PlatformMain, DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+
+
     @Override
     public Logger getLogger() {
         return null;
