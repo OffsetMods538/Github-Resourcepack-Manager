85fb33d4e20498bf13c66837563317313b36c60e
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-01-29 17:19:37 +0200

    Make GitPackManagerException use ArgReplacer, add constructor overloads for ArgReplacer overloads

--- common/src/main/java/top/offsetmonkey538/gitpackmanager/exception/GitPackManager.java
+++ common/src/main/java/top/offsetmonkey538/gitpackmanager/exception/GitPackManager.java
@@ -1,19 +1,37 @@
 package top.offsetmonkey538.gitpackmanager.exception;
 
+import static top.offsetmonkey538.offsetutils538.api.text.ArgReplacer.replaceArgs;
+
 public class GitPackManager extends Exception {
     public GitPackManager(String message) {
         super(message);
     }
 
+    public GitPackManager(String message, Object arg) {
+        this(replaceArgs(message, arg));
+    }
+
+    public GitPackManager(String message, Object arg1, Object arg2) {
+        this(replaceArgs(message, arg1, arg2));
+    }
+
     public GitPackManager(String message, Object... args) {
-        this(String.format(message, args));
+        this(replaceArgs(message, args));
     }
 
     public GitPackManager(String message, Throwable cause) {
         super(message, cause);
     }
 
+    public GitPackManager(String message, Throwable cause, Object arg) {
+        this(replaceArgs(message, arg), cause);
+    }
+
+    public GitPackManager(String message, Throwable cause, Object arg1, Object arg2) {
+        this(replaceArgs(message, arg1, arg2), cause);
+    }
+
     public GitPackManager(String message, Throwable cause, Object... args) {
-        this(String.format(message, args), cause);
+        this(replaceArgs(message, args), cause);
     }
 }
